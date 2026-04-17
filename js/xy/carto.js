43a2942b9a8647f33b0b7bb47e24b6274300e9a9
probablyanocelot <58635071+probablyanocelot@users.noreply.github.com>
2026-04-16 20:34:19 -0500

    feat: implement Grapher class with plotting and bounds checking functionality

---
+++ js/xy/carto.js
@@ -0,0 +1,38 @@
+const { LiveClass } = require("../obj/index.js");
+
+const plane = {
+  bounds: {
+    x: [-100, 100],
+    y: [-100, 100],
+  },
+};
+
+const Grapher = LiveClass(plane);
+
+Grapher.set("points", []);
+
+Grapher.set("isInBounds", function (x, y) {
+  const xInBounds = this.bounds.x[0] <= x && x <= this.bounds.x[1];
+  const yInBounds = this.bounds.y[0] <= y && y <= this.bounds.y[1];
+  return { x: xInBounds, y: yInBounds };
+});
+
+Grapher.set("plot", function (x, y) {
+  const result = { pass: false };
+  if (!this.isInBounds(x, y).x || !this.isInBounds(x, y).y) {
+    result.x = this.isInBounds(x, y).x;
+    result.y = this.isInBounds(x, y).y;
+    return result;
+  }
+
+  result.pass = true;
+  result.x = x;
+  result.y = y;
+  const point = [x, y];
+  this.points.push(point);
+  return result;
+});
+console.log(Grapher.plot(850, 50));
+console.log(Grapher.points);
+console.log(Grapher);
+module.exports = { Grapher };
